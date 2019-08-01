4310dac0299da67e9d7fd83db2867dd59794dd43
Navneet Prakash <hello_navneet@hotmail.com>
2019-08-01 14:21:47 +0530

    Creates JWT Toekn and sends to FrontEnd in cookie as SESSIONID

--- server/security.utils.ts
+++ server/security.utils.ts
@@ -1,22 +1,23 @@
-
-
-
 import moment = require("moment");
-const util = require('util');
-const crypto = require('crypto');
-import * as jwt from 'jsonwebtoken';
+const util = require("util");
+const crypto = require("crypto");
+import * as jwt from "jsonwebtoken";
 import * as fs from "fs";
-
-
 
 export const randomBytes = util.promisify(crypto.randomBytes);
 
+const signJwt = util.promisify(jwt.sign);
 
+const RSA_PRIVATE_KEY = fs.readFileSync("./demos/private.key");
 
-const RSA_PRIVATE_KEY = fs.readFileSync('./demos/private.key');
-
-const RSA_PUBLIC_KEY = fs.readFileSync('./demos/public.key');
+const RSA_PUBLIC_KEY = fs.readFileSync("./demos/public.key");
 
 const SESSION_DURATION = 240;
 
-
+export function createSessionToken(userId: string) {
+  return jwt.sign({}, RSA_PRIVATE_KEY, {
+    algorithm: "RS256",
+    expiresIn: SESSION_DURATION,
+    subject: userId
+  });
+}
